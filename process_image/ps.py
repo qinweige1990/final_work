13e0e0b784a0bf1459824754e995b14d434b23cf
qinweige <qinweige@bytedance.com>
2024-02-27 18:10:45 +0800

    feat: change panda version

--- process_image/ps.py
+++ process_image/ps.py
@@ -1,5 +1,8 @@
 # Import third-party modules
 import os
+import random
+import string
+
 # Import local modules
 from photoshop import Session
 from psd_tools import PSDImage
@@ -18,7 +21,7 @@ def change_size(dir, photo_name, width, height):
     this_root = os.path.dirname(__file__)
     photo_dir = os.path.join(this_root, f"photos/{dir}")
     photo_path = os.path.join(photo_dir, photo_name)
-    with Session(photo_path, action="open") as ps:
+    with Session(photo_path, action="open", auto_close=True) as ps:
         resized_photo_path = os.path.join(this_root, f"resized_photos/{dir}")
         os.makedirs(resized_photo_path, exist_ok=True)
         output_path = os.path.join(resized_photo_path, photo_name)
@@ -34,9 +37,12 @@ def change_size(dir, photo_name, width, height):
             print("not windows, pass resize")
     return output_path
 
+
 def replace_and_save_psd(photo, psd_file, name):
-    with Session(psd_file, action="open") as ps:
-        for active_layer in ps.active_document.layerSets:
+    with Session(psd_file, action="open", auto_close=True) as ps:
+        orig_name = ps.active_document.name
+        doc = ps.active_document.duplicate(f'{orig_name}_process')
+        for active_layer in doc.layerSets:
             # layer_set = ps.active_document.layerSets.getByName("画板 9")
             print(f"current layer {active_layer.name}")
             replace_contents = ps.app.stringIDToTypeID("placedLayerReplaceContents")
@@ -44,9 +50,11 @@ def replace_and_save_psd(photo, psd_file, name):
             idnull = ps.app.charIDToTypeID("null")
             desc.putPath(idnull, photo)
             ps.app.executeAction(replace_contents, desc)
-    psd_path = os.path.join(os.path.dirname(__file__), f"files/{psd_file}")
-    all_in_one_photo = os.path.join(os.path.dirname(__file__), f"final_photos/{name}.png")
-    split_photos_dir = os.path.join(os.path.dirname(__file__), f"final_photos/{name}")
+        psd_output = f'{psd_file}_with_{name}_{"".join(random.choices(string.ascii_letters,k=5))}'
+        doc.saveAs(psd_output, ps.PhotoshopSaveOptions(), asCopy=True)
+    psd_path = os.path.join(os.path.dirname(__file__), f"files/{psd_output}")
+    all_in_one_photo = os.path.join(os.path.dirname(__file__), f"final_photos/{psd_output}.png")
+    split_photos_dir = os.path.join(os.path.dirname(__file__), f"final_photos/{psd_output}")
     psd = PSDImage.open(psd_path)
     psd.composite().save(all_in_one_photo)
     for layer in psd:
