441d2e9c9acfac4788933b76be411e1166a0edf3
qinweige <qinweige@bytedance.com>
2024-03-26 20:51:08 +0800

    feat: add new check

--- i_replace_image.py
+++ i_replace_image.py
@@ -8,5 +8,8 @@ if __name__ == "__main__":
     parser.add_argument("--psd", type=str)
     parser.add_argument("--name", type=str)
     args = parser.parse_args()
-
+    if args.input is None:
+        args.input = input("输入元素所在的文件夹：")
+        args.psd = input("输入模版位置：")
+        args.name = input("输入保存后的名称：")
     ps.replace_and_save_psd(args.input, args.psd, args.name)
